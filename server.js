94bc54b7abc8193e4ee10f7accdd4de0830e4a2f
stuart-hahn <47613093+stuart-hahn@users.noreply.github.com>
2024-11-02 14:32:30 -0400

    feat: add middleware to log requests

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 3030;
 
+// middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
 app.get("/", (req, res) => {
   res.json({ message: "Mental Health Journal" });
 });
